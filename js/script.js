01f27bd5e0d16787e40d562311da945b47f1b063
wiejak555 <wiejak555@gmail.com>
2019-12-08 12:49:46 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+function titleClickHandler(event) {
+    console.log('Link was clicked!');
+    console.log(event);
+    event.preventDefault();
+    /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
+    /* add class 'active' to the clicked link */
+    const clickedElement = this;
+
+    clickedElement.classList.add('active');
+
+    console.log('clickedElement:', clickedElement);
+
+    /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.post');
+
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
+    /* get 'href' attribute from the clicked link */
+
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
+
+    /* find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+    /* add class 'active' to the correct article */
+
+    targetArticle.classList.add('active');
+
+}
+
+
+
+const links = document.querySelectorAll('.titles a');
+
+for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+}
